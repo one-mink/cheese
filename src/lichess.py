90b7663f1b04a290b8eeaefbe24f222e61c4328d
one-mink <dsoe.leon@gmail.com>
2026-02-12 20:21:26 +0100

    Add files via upload

---
+++ src/lichess.py
@@ -0,0 +1,22 @@
+import berserk
+
+session = berserk.TokenSession('LICHESS API TOKEN')
+client = berserk.Client(session=session)
+
+email = client.account.get_email()
+
+def play_game():
+    client.challenges.create(username="cheese_bot2", rated=False)
+    challenges = client.challenges.get_mine()
+    if challenges:
+        c_id = challenges['out'][0]['id']
+        return c_id
+
+def move(c_id, place):
+    client.board.make_move(c_id, place)
+    return "move ok"
+
+def accept_challenge(c_id):
+    session_op = berserk.TokenSession('ENEMY API TOKEN')
+    client_op = berserk.Client(session=session_op)
+    client_op.challenges.accept(c_id)
---
+++ src/read.py
@@ -0,0 +1,18 @@
+import binascii
+
+def read(file):
+   with open(file, "rb") as image:
+      f = image.read()
+      data = bytearray(f)
+   return data
+
+raw_data = read('banana.png')
+
+def translate(data):
+    bin = []
+    for byte in data:
+        bin.append(byte // 20)
+        bin.append(byte % 20)
+    return bin
+
+print(translate(raw_data))
