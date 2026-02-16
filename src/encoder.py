dc429f0da6bc3ee9bf384b31e06fdc220125637a
one-mink <dsoe.leon@gmail.com>
2026-02-16 13:04:22 +0100

    Updated encoder.py
    Every element in the Binary data will be print out as a chess move now.

--- src/encoder.py
+++ src/encoder.py
@@ -3,22 +3,37 @@ import chess
 import lichess
 
 board = chess.Board()  #Creates virtual chess board
-c_id = 'o3mPJWONBcGe'#lichess.get_c_id() #gets game id from lichess
+c_id = lichess.play_game() #gets game id from lichess
 data = read.bin_data #binary data of the image
 moves = [move.uci() for move in board.legal_moves] #checks legal moves
+game_num = 1
 
+games = [] #game id of played games
 print(board)
+
+#test_data = [0, 8, 14, 3, 6, 4, 16, 6, 10]
 
 def move(move):
     board.push_uci(move)
+    lichess.move(c_id, move)
+
+def move_b(move):
+    board.push_uci(move)
+    lichess.move_b(c_id, move)
+
 
 def encode(data):
    global moves
    global board
-   for number in data:
-       e = data[number]
+   global c_id
+   global game_num
+
+   for e in data:
        if not moves or board.is_game_over():
            board.reset()
+           c_id = lichess.play_game()
+           game_num += 1
+           games.append(c_id)
            moves = [move.uci() for move in board.legal_moves]
 
        if e < len(moves):
@@ -27,20 +42,20 @@ def encode(data):
 
        else:
            m = moves[0]
-       move(m)
+
+       try:
+          if board.turn == chess.WHITE:
+             move(m)
+          elif board.turn == chess.BLACK:
+              move_b(m)
+       except:
+           moves = []
+           continue
        moves = [move.uci() for move in board.legal_moves]
 
        print(board)
        print(moves)
 
 print(c_id)
-
-    #lichess.move_b(c_id, move)
-
-#board.push_uci("0000")
-
-#move("a2a3")
-
 encode(data)
-print(board)
-print(moves)
+print(game_num)
--- src/lichess.py
+++ src/lichess.py
@@ -9,13 +9,10 @@ client_op = berserk.Client(session=session_op)
 email = client.account.get_email()
 
 def play_game():
-    client.challenges.create(username="cheese_bot2", rated=False, color="white")
-
-def get_c_id():
-        challenges = client.challenges.get_mine()
-        if challenges:
-            c_id = challenges['out'][0]['id']
-            return c_id
+    challenge = client.challenges.create(username="cheese_bot2", rated=False, color="white")
+    c_id = challenge['id']
+    client_op.challenges.accept(c_id)
+    return c_id
 
 def move(c_id, place):
     client.board.make_move(c_id, place)
@@ -24,9 +21,6 @@ def move(c_id, place):
 def move_b(c_id, place):
     client_op.board.make_move(c_id, place)
     return "b move ok"
-
-def accept_challenge(c_id):
-    client_op.challenges.accept(c_id)
 
 def get_board(c_id):
     board_state = client.board.stream_game_state(c_id)
